94629f7a193a24b062d08b323da9954082ab482f
lguertin <leandre.guertin@polymtl.ca>
2019-11-05 23:27:59 -0500

    Added moves14

--- TP2/py/main.py
+++ TP2/py/main.py
@@ -47,5 +47,27 @@ def moves_16():
     print('\nEND - Nb moves: ', algo.nb_moves_tot,'\n')
     # %time
 
+def moves_14():
+    # solution optimale: 14 moves
+    rh = Rushhour([True, False, True, False, False, False, True, True, False, True, True],
+                    [2, 2, 3, 2, 2, 3, 3, 2, 2, 2, 2],
+                    [2, 0, 0, 3, 4, 5, 3, 5, 2, 5, 4],
+                    ["rouge", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+    s = State([0, 0, 3, 1, 2, 1, 0, 0, 4, 3, 4])
+    algo = MiniMaxSearch(rh, s,3)
+    algo.rushhour.init_positions(s)
+    
+    print('Init: ')
+    # print(algo.rushhour.free_pos)
+    algo.rushhour.print_pretty_grid(s)
+    print('===')
 
-moves_16()
+    algo.solve(s, False)
+    print("===")
+    print('Last grid:')
+    algo.rushhour.print_pretty_grid(algo.state)
+    print('\nEND - Nb moves: ', algo.nb_moves_tot,'\n')
+    # %time
+
+
+moves_14()
